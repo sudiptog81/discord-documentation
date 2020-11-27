b3a4910a344d48e7043104c1a05a75112994e188
nhcarrigan <nhcarrigan@gmail.com>
2020-11-27 12:16:16 -0800

    Add art name
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- src/assets/data/artList.ts
+++ src/assets/data/artList.ts
@@ -1,5 +1,6 @@
 export interface artInt {
   file_name: string;
+  art_name: string;
   artist: string;
   artist_url: string;
 }
@@ -7,86 +8,103 @@ export interface artInt {
 export const artList: artInt[] = [
   {
     file_name: 'Moonlight 1.png',
+    art_name: 'Becca Lyria',
     artist: 'Moonlight',
     artist_url: 'https://www.instagram.com/moonlightkcreations/',
   },
   {
     file_name: 'Moonlight 3.png',
+    art_name: 'A Forest Adventure',
     artist: 'Moonlight',
     artist_url: 'https://www.instagram.com/moonlightkcreations/',
   },
   {
     file_name: 'Moonlight 4.png',
+    art_name: 'A Day at the Beach',
     artist: 'Moonlight',
     artist_url: 'https://www.instagram.com/moonlightkcreations/',
   },
   {
     file_name: 'Moonlight 5.png',
+    art_name: 'Princess Becca',
     artist: 'Moonlight',
     artist_url: 'https://www.instagram.com/moonlightkcreations/',
   },
   {
     file_name: 'Moonlight 6.jpg',
+    art_name: 'Sketch',
     artist: 'Moonlight',
     artist_url: 'https://www.instagram.com/moonlightkcreations/',
   },
   {
     file_name: 'picrew1.png',
+    art_name: 'Playful',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew2.png',
+    art_name: 'Caught in the Woods',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew3.png',
+    art_name: 'An Evening Out',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew4.png',
     artist: 'picrew.me',
+    art_name: 'An Evening In',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew5.png',
+    art_name: 'Just Another Day',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew6.png',
+    art_name: 'Portrait',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew7.png',
+    art_name: 'Feeling Flirty',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew8.png',
+    art_name: 'Love 1',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew9.png',
+    art_name: 'Love 2',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew10.png',
+    art_name: 'A Night in the Town',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew11.png',
+    art_name: 'Love 3',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
   {
     file_name: 'picrew12.png',
+    art_name: 'Love 4',
     artist: 'picrew.me',
     artist_url: 'https://picrew.me/',
   },
