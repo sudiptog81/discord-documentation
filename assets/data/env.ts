3d72d5f050990da5647dc4d0c323f24faf0183f4
nhcarrigan <nhcarrigan@gmail.com>
2020-11-26 20:46:27 -0800

    Auto-generated commit

--- assets/data/env.ts
+++ assets/data/env.ts
@@ -9,7 +9,7 @@ export const envVariables: envVariableInterface[] = [
     name: 'DISCORD_TOKEN',
     optional: 'REQUIRED',
     description:
-      'The value of this variable will be your Bot Token from the Discord developer page. It is VERY important that you keep this token a secret - do not upload it to GitHub or share it publicly, as this will allow anyone to access your discord bot.',
+      'The value of this variable will be your Token from the Discord developer page. It is VERY important that you keep this token a secret - do not upload it to GitHub or share it publicly, as this will allow anyone to access your Discord Bot application.',
   },
   {
     name: 'HABITICA_KEY',
@@ -27,7 +27,7 @@ export const envVariables: envVariableInterface[] = [
     name: 'MONGODB',
     optional: 'REQUIRED',
     description:
-      'The value of this variable will be your MongoDB authentication URI. Like the bot token, this needs to be kept secret to prevent unauthorised access to and modification of your database.',
+      'The value of this variable will be your MongoDB authentication URI. Like the token, this needs to be kept secret to prevent unauthorised access to and modification of your database.',
   },
   {
     name: 'NASA_API',
@@ -39,22 +39,22 @@ export const envVariables: envVariableInterface[] = [
     name: 'OWNER_ID',
     optional: 'OPTIONAL',
     description:
-      'This tells the bot which Discord account is the owner. The bot will respond just a little differently to the owner. Leaving this blank WILL lock out some commands.',
+      'This tells Becca which Discord account is her owner. She will respond just a little differently to the owner. Leaving this blank WILL lock out some commands.',
   },
   {
     name: 'PRODDEV',
     optional: 'OPTIONAL',
     description:
-      'This tells the bot whether it is in a development state or production state - the webhook message includes this value.',
+      'This tells Becca whether she is in a development state or production state - the webhook message includes this value.',
   },
   {
     name: 'WH_ID',
     optional: 'RECOMMENDED',
-    description: 'This is the ID for the Discord webhook. The bot will log when it goes online, connects to the database, and any time a server has an error. This is not a required value, but highly recommended.',
+    description: 'This is the ID for the Discord webhook. Becca will log when it goes online, connects to the database, and any time a server has an error. This is not a required value, but highly recommended.',
   },
   {
     name: 'WH_TOKEN',
     optional: 'RECOMMENDED',
-    description: 'This is the token for the Discord webhook. The bot will log when it goes online, connects to the database, and any time a server has an error. This is not a required value, but highly recommended.',
+    description: 'This is the token for the Discord webhook. Becca will log when it goes online, connects to the database, and any time a server has an error. This is not a required value, but highly recommended.',
   },
 ];
