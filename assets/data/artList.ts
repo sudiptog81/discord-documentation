1ed112e602f6b0893fe6957b228f1c34b4ebb8a1
nhcarrigan <nhcarrigan@gmail.com>
2020-11-26 17:21:57 -0800

    Auto-generated commit

---
+++ assets/data/artList.ts
@@ -0,0 +1,93 @@
+export interface artInt {
+  file_name: string;
+  artist: string;
+  artist_url: string;
+}
+
+export const artList: artInt[] = [
+  {
+    file_name: 'Moonlight 1.png',
+    artist: 'Moonlight',
+    artist_url: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    file_name: 'Moonlight 3.png',
+    artist: 'Moonlight',
+    artist_url: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    file_name: 'Moonlight 4.png',
+    artist: 'Moonlight',
+    artist_url: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    file_name: 'Moonlight 5.png',
+    artist: 'Moonlight',
+    artist_url: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    file_name: 'Moonlight 6.jpg',
+    artist: 'Moonlight',
+    artist_url: 'https://www.instagram.com/moonlightkcreations/',
+  },
+  {
+    file_name: 'picrew1.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew2.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew3.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew4.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew5.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew6.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew7.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew8.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew9.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew10.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew11.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+  {
+    file_name: 'picrew12.png',
+    artist: 'picrew.me',
+    artist_url: 'https://picrew.me/',
+  },
+];
